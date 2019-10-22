0449f573db4169b5593751f422728d8752846430
Dakota <dakota@bengerlaw.com>
2019-10-22 14:25:17 -0700

    first commit (front end)

---
+++ main.js
@@ -0,0 +1,26 @@
+function validateForm() {
+    var x = document.forms["survey-form"]["name"]["email"]["age"]["dob"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
+  //Get the button for jump to the top:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
